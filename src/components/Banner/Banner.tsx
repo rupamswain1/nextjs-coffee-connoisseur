ed64f3ea194be843c731d1da1dc118a4cf3dc226
rupam swain <rupamswain1@gmail.com>
2023-11-03 23:38:55 +0530

    banner component styling

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -7,10 +7,10 @@ const Banner: React.FunctionComponent<IBannerProps> = ({
   onButtonPress,
 }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.bannerContainer}>
       <h1 className={styles.title}>
-        <span className={styles.title}>Coffee</span>
-        <span className={styles.title}>Connoisseur</span>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Connoisseur</span>
       </h1>
       <p className={styles.subtitle}>Discover Your Local Coffee Shops!</p>
       <button className={styles.button} onClick={onButtonPress}>
